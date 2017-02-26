e381330fd87e7689b54201ee5066ef3ddfd5b7c3
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2017-02-26 22:21:52 +0000

    Configuration and schema paths CWD-relative

--- che.js
+++ che.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 var fs      = require('fs'),
  Handlebars = require('handlebars'),
+ path       = require('path'),
  pointer    = require('json-pointer-rfc6901');
 
-var conf    = require(process.argv[2]),
+var conf    = require(path.join(process.cwd(), process.argv[2])),
  generators = conf.generators,
  helpers    = require('./helpers');
 
-var schema = require(process.argv[3]);
+var schema = require(path.join(process.cwd(), process.argv[3]));
 
 Handlebars.registerHelper('toCamelCase',  helpers.toCamelCase);
 Handlebars.registerHelper('toKebabCase',  helpers.toKebabCase);
