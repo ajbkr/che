d6d756430e621669f1b76e1ba282bf523093d893
Andrew J. Baker <andrew_j_baker2@hotmail.com>
2020-01-03 14:38:23 +0000

    Deprecate helpers in favour of new compile function

--- che.js
+++ che.js
@@ -1,17 +1,14 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const pointer = require('json-pointer-rfc6901')
-const _ = { template: require('lodash.template') }
 const path = require('path')
 
-const { generators, helpers, schema } =
+const { compile, generators, schema } =
   require(path.join(process.cwd(), process.argv[2]))
-
-const imports = { imports: helpers }
 
 for (const ptr in generators) {
   for (const templateFile in generators[ptr]) {
-    const template = _.template(fs.readFileSync(templateFile).toString(), imports)
+    const template = compile(fs.readFileSync(templateFile).toString())
 
     const output = template(pointer.get(schema, ptr))
 
