316dd00a4c22364e12d0e5adfba4e9f7c95c6612
Andrew Baker <andrew_j_baker2@hotmail.com>
2021-01-16 19:33:56 +0000

    Executable example

---
+++ example/che.conf.js
@@ -0,0 +1,37 @@
+const template = require('lodash.template')
+
+const helpers = require('template-helpers')('string')
+
+const compile = string => template(string, { imports: helpers })
+
+const schema = {
+  donaldDuck: {
+    nephews: [
+      {
+        name: 'huey',
+        color: 'red'
+      },
+      {
+        name: 'dewey',
+        color: 'blue'
+      },
+      {
+        name: 'louie',
+        color: 'green'
+      }
+    ],
+    title: "Donald Duck's Nephews"
+  }
+}
+
+const generators = {
+  '/donaldDuck': {
+    'example/index.jst': 'example/index.html'
+  }
+}
+
+module.exports = {
+  compile,
+  generators,
+  schema
+}
